3ebc37fd86bb28c6800b8113dc449e59499fb6c9
sergey <sergeylakki@mail.ru>
2022-01-04 00:22:06 +0300

    Создал класс message

---
+++ ConsoleClient/Class1.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace MyMessanger_stepik
+{
+    public class Message
+    { 
+        public string UserName { get; set; }
+        public string MessageText { get; set; }
+        public DateTime TimeStamp { get; set; }
+        
+        public Message(string userName, string messageText, DateTime timeStamp)
+        {
+            UserName = userName;
+            MessageText = messageText;
+            TimeStamp = timeStamp;
+        }  
+        
+        public Message()
+        {
+            UserName = "System";
+            MessageText = "Server is running..";
+            TimeStamp = DateTime.Now;
+        }
+
+        public override string ToString()
+        {
+            string str = String.Format("{0} <{2}>:{1}", UserName, MessageText, TimeStamp);
+            return str;
+        }
+    }
+
+
+}
--- ConsoleClient/Program.cs
+++ ConsoleClient/Program.cs
@@ -6,7 +6,8 @@ namespace MyMessanger_stepik
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            Message ms = new Message();
+            Console.WriteLine(ms.ToString());
         }
     }
 }
