75c93f40ccbdcaa6b9bd9272cb362f4b63ec7ce1
sergey <sergeylakki@mail.ru>
2022-01-03 23:48:29 +0300

    Добавьте файлы проекта.

---
+++ Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace MyMessanger_stepik
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
